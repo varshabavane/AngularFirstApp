8c1eddc535899c367065024e4e0e4caa9adc49de
varsha bavane <varshab13bavane@gmail.com>
2018-10-24 15:48:49 +0530

    angular manterial integrated

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// For Geasture support;
+import 'hammerjs';
+
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
