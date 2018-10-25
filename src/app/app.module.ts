b4e82df5756cb6d5a8b84738f4c2fd2ebbf7d2de
varsha bavane <varshab13bavane@gmail.com>
2018-10-25 09:27:01 +0530

    list-module integratd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//AppMaterial module imported;
 import {AppMaterialModule}  from './appmaterial.module';
 // for animations
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+//import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +19,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AppMaterialModule
+    AppMaterialModule,
+    
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/appmaterial.module.ts
+++ src/app/appmaterial.module.ts
@@ -1,9 +1,50 @@
-import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { NgModule } from '@angular/core';
+//datepicker
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material';
+//for input box
+import { MatInputModule } from '@angular/material/input';
+//for form
+import { MatFormFieldModule } from '@angular/material/form-field';
+//for slide toggle button
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatSelectModule} from '@angular/material/select';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatDividerModule} from '@angular/material/divider';
+//to show list of items
+import {MatListModule} from '@angular/material/list';
+
+
+
+
+
 @NgModule({
-    imports:[MatCheckboxModule],
-    exports:[MatCheckboxModule]
+    imports: [
+        MatCheckboxModule,
+        MatMenuModule,
+        MatDatepickerModule,
+        MatListModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatNativeDateModule,
+        MatSlideToggleModule,
+        MatSelectModule,
+        MatDividerModule
+
+    ],
+    exports: [
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSlideToggleModule,
+        MatNativeDateModule,
+        MatSelectModule,
+        MatMenuModule,
+        MatDividerModule,
+        MatListModule]
 })
 
 
-export class AppMaterialModule {}
+export class AppMaterialModule { }
